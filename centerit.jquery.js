849d7139f1900ec7b559b8b3f119c8c3239b391b
Muhammed AK <mastersdream@gmail.com>
2016-03-17 21:54:54 +0530

    implementing Immediately Invoked Function Expression

--- centerit.jquery.js
+++ centerit.jquery.js
@@ -1,17 +1,19 @@
-$.fn.centerit = function() {
-  var elem = $(this);
-  var wind = $(window)
-  var imgHeight = elem.height();
-  var imgWidth = elem.width();
-  var wWidth = wind.width();
-  var wHeight = wind.height();
+(function($) {
+  $.fn.centerit = function() {
+    var elem = $(this);
+    var wind = $(window)
+    var imgHeight = elem.height();
+    var imgWidth = elem.width();
+    var wWidth = wind.width();
+    var wHeight = wind.height();
 
-  // Apply CSS to make this element centerit
-  // absolute element with 50% left and top value sustracted by respective elem width/2
-elem.css({
-  'position':'absolute',
-  'left':wWidth/2  - imgWidth/2,
-  'top':wHeight/2  - imgHeight/2,
-})
+    // Apply CSS to make this element centerit
+    // absolute element with 50% left and top value sustracted by respective elem width/2
+    elem.css({
+      'position': 'absolute',
+      'left': wWidth / 2 - imgWidth / 2,
+      'top': wHeight / 2 - imgHeight / 2,
+    })
 
-};
+  };
+}(jQuery));
