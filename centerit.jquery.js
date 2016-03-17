1d6e5f27992d70f5f2eddada452215fb93092982
Muhammed AK <mastersdream@gmail.com>
2016-03-17 22:10:03 +0530

    Little OOP way

--- centerit.jquery.js
+++ centerit.jquery.js
@@ -1,5 +1,7 @@
 (function($) {
   $.fn.centerit = function() {
+
+
     var elem = $(this);
     var wind = $(window)
     var imgHeight = elem.height();
@@ -7,13 +9,18 @@
     var wWidth = wind.width();
     var wHeight = wind.height();
 
+    makeCenter();
+
     // Apply CSS to make this element centerit
     // absolute element with 50% left and top value sustracted by respective elem width/2
-    elem.css({
-      'position': 'absolute',
-      'left': wWidth / 2 - imgWidth / 2,
-      'top': wHeight / 2 - imgHeight / 2,
-    })
+
+    function makeCenter() {
+      elem.css({
+        'position': 'absolute',
+        'left': wWidth / 2 - imgWidth / 2,
+        'top': wHeight / 2 - imgHeight / 2,
+      })
+    }
 
   };
 }(jQuery));
