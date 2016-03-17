c468a135bb5340b61f93a7020ffe47d9fb241170
Muhammed AK <mastersdream@gmail.com>
2016-03-17 22:29:04 +0530

    update centerit when window resize

--- centerit.jquery.js
+++ centerit.jquery.js
@@ -1,26 +1,30 @@
-(function($) {
+(function($) { //Immediately-Invoked Function Expression (IIFE)
   $.fn.centerit = function() {
 
+    var elem = $(this),
+      wind = $(window);
 
-    var elem = $(this);
-    var wind = $(window)
-    var imgHeight = elem.height();
-    var imgWidth = elem.width();
-    var wWidth = wind.width();
-    var wHeight = wind.height();
+    var makeCenter = function() {
+      var imgHeight = elem.height(),
+        imgWidth = elem.width(),
+        wWidth = wind.width(),
+        wHeight = wind.height();
 
-    makeCenter();
-
-    // Apply CSS to make this element centerit
-    // absolute element with 50% left and top value sustracted by respective elem width/2
-
-    function makeCenter() {
+      // Apply CSS to make this element centerit
+      // absolute element with 50% left and top value sustracted by respective elem width/2
       elem.css({
         'position': 'absolute',
         'left': wWidth / 2 - imgWidth / 2,
         'top': wHeight / 2 - imgHeight / 2,
       })
     }
+
+    // Change position when window get resized
+    wind.resize(function() {
+      makeCenter();
+    });
+    // make it center by calling makeCenter function
+    makeCenter(); 
 
   };
 }(jQuery));
